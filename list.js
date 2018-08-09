2f5dd28261bcd2f9b29ab2db6b8ae377b58bb455
Brad Ledbetter <bradley.ledbetter@e-hps.com>
2018-08-09 14:14:25 -0400

    Completing refactor to promises

--- list.js
+++ list.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
 const path = require('path');
 const moment = require('moment');
 const sharp = require('sharp');
@@ -6,34 +7,6 @@ const PptxGenJs = require('pptxgenjs');
 const MAX_IMAGE_WIDTH_PX = 958;
 const MAX_IMAGE_HEIGHT_PX = 540;
 const todayStamp = moment().format('YYYY-MM-DD');
-
-/**
- * Write the movie names and slide list files
- * @param {string} category game category
- * @param {string} movieNames text for movie name file
- * @param {string} slideList text for slidelist file
- * @param {function(Array<string>)} callback call this when done so we can make a pptx
- */
-function writeSlideCallSheet(slideMap, callback) {
-    let slideList = '';
-    // iterate over the map
-    slideMap.forEach((slides, category) => {
-        str += category + '\n';
-        slides.forEach(slide => str += slide + '\n');
-        str += '\n';
-    });
-
-    fs.writeFile(`slide-list-${todayStamp}.txt`, slideList, (err) => {
-        if (err) {
-            console.error(err);
-            process.exit();
-        }
-
-        if (callback && typeof callback === 'function') {
-            callback(slideList);
-        }
-    });
-}
 
 /**
  * Convert a string to title case
@@ -48,17 +21,23 @@ function titleCase(txt) {
 }
 
 /**
- * Remove duplicates from an array of strings
- * @param {Array<string>} items
+ * Check whether this is a valid slide filename
+ * @param {string} filename
+ * @returns {boolean}
  */
-function removeDuplicates(items) {
-    let returnItems = [];
-    items.forEach((item) => {
-        if (!returnItems.includes(item) && item !== 'desktop.ini') {
-            returnItems.push(item);
-        }
-    });
-    return returnItems;
+function isFilenameOK(filename) {
+    return filename.match(/\.(png|jpg|gif)$/) !== null && filename[ 0 ] !== '.';
+}
+
+/**
+ * Remove duplicates from an array of strings, ignores non images and system files
+ * @param {string[]} fileList
+ */
+function cleanUpFileList(fileList) {
+    return fileList
+        .reduce((ra, filename) => { return ra.includes(filename) ? ra : ra.concat(filename); }, []) // remove duplicates
+        .map(filename => isFilenameOK(filename) ? filename : false) // make sure the filename is acceptable
+        .filter(item => item); // filter out empties
 }
 
 /**
@@ -71,26 +50,16 @@ function limitArrayTo(ra, len) {
 }
 
 /**
- * Clean up individual filenames and eliminate duplicates
- * @param {Array} filenames
- * @return {Array}
+ * Turn "movie name.jpg" to "Movie Name"
+ * @param {string} filename
+ * @returns {string}
  */
-function filterFilenames(filenames) {
-    // format the names
-    let lines = [];
-    filenames.forEach((filename) => {
-        // only include image files
-        if (filename.match(/\.(png|jpg|gif)$/) !== null) {
-            let line = filename.substr(0, filename.lastIndexOf('.'));
-            line = titleCase(line);
-            lines.push(line + '\n');
-        }
-    });
-
-    // sort alphabetically
-    lines.sort();
-
-    return lines;
+function filenameToMovie(filename) {
+    let newName = filename.substr(0, filename.lastIndexOf('.'));
+    newName = titleCase(newName);
+    return newName
+        .replace(/[\(\)]/g, ' - ')
+        .replace(/_/g, '');
 }
 
 /**
@@ -110,9 +79,9 @@ function fyShuffle(iArray) {
         currentIndex -= 1;
 
         // And swap it with the current element.
-        temporaryValue = iArray[currentIndex];
-        iArray[currentIndex] = iArray[randomIndex];
-        iArray[randomIndex] = temporaryValue;
+        temporaryValue = iArray[ currentIndex ];
+        iArray[ currentIndex ] = iArray[ randomIndex ];
+        iArray[ randomIndex ] = temporaryValue;
     }
 
     return iArray;
@@ -121,97 +90,61 @@ function fyShuffle(iArray) {
 /**
  * Get a list of the directories in this directory.
  * The assumption being that the only thing in those directories are movie stills
- * @param {function(Array<string>)} callback function to call with directory names
+ * @param {Promis<string[]>} function to call with directory names
  */
-function getDirectories(callback) {
-    fs.readdir('.', (err, dirItems) => {
-        if (err) {
-            console.log(err);
-            process.exit();
-        }
-
-        const ignoreDirs = ['.', '..', 'node_modules'];
-        const directories = [];
-        dirItems.forEach((dirItem) => {
-            // ignore certain directories, and hidden folders
-            if (!ignoreDirs.includes(dirItem) && dirItem.match(/^\./) === null) {
-                const stats = fs.statSync(dirItem);
-                if (stats.isDirectory()) {
-                    directories.push(dirItem);
+function getDirectories() {
+    return fs.readdir('.')
+        .then(dirItems => {
+            const ignoreDirs = [ '.', '..', 'node_modules' ];
+            const directories = [];
+            dirItems.forEach((dirItem) => {
+                // ignore certain directories, and hidden folders
+                if (!ignoreDirs.includes(dirItem) && dirItem.match(/^\./) === null) {
+                    const stats = fs.statSync(dirItem);
+                    if (stats.isDirectory()) {
+                        directories.push(dirItem);
+                    }
                 }
-            }
-        });
+            });
 
-        if (callback && typeof callback === 'function') {
-            callback(directories);
-        }
-    });
+            return directories;
+        })
+        .catch(err => {
+            console.error('Unable to read directory/ies: ', err);
+            process.exit();
+        });
 }
 
-const allSlidePromises = [];
+/**
+ * Write the movie names and slide list files
+ * @param {string} category game category
+ * @param {string} movieNames text for movie name file
+ * @param {string} slideList text for slidelist file
+ * @returns {Promise<Map<string, object[]>>} return the slideMap when we're done writing the file
+ */
+function writeSlideCallSheet(slideMap) {
+    let slideList = '';
+    // iterate over the map
+    slideMap.forEach((slides, category) => {
+        slideList += category + '\n';
+        slides.forEach(slide => slideList += slide.movieName + '\n');
+        slideList += '\n';
+    });
+
+    return fs.writeFile(`slide-list-${todayStamp}.txt`, slideList)
+        .then(() => slideMap)
+        .catch(err => {
+            console.error('Error writing call sheet: ', err);
+            process.exit();
+        });
+}
 
 /**
  * Add to pptx slideshow
  * @param {Map<string, string[]>} slideMap
- * @param {object} pptx PPTX instance
+ * @returns {Promise<PptxGenJs>} resolves with PptxGenJs instance
  */
-function writePptx(slideMap, pptx) {
-    slideMap.forEach((slides, category) => {
-        // add round title
-        const titleSlide = pptx.addNewSlide('MAIN');
-        titleSlide.addText(category); // TODO:
-
-        slides.forEach((slideFilename, slideIndex) => {
-            if (slideFilename.match(/\.(png|jpg|gif)$/) !== null) {
-                const slidePath = path.join('.', category, slideFilename);
-                console.log(`${category}, ${slideIndex}: ${slidePath}`);
-                const slide = pptx.addNewSlide('MAIN');
-
-                // get image size synchronously. Probably slow, but easy to code.
-                const image = sharp(slidePath);
-                image.metadata((err, imgData) => {
-                    if (err) {
-                        console.error(err);
-                        process.exit();
-                    }
-
-                    // NOTE: we need to include the original width and height for the aspect ratio to be maintained
-                    slide.addImage({
-                        path: slidePath,
-                        x: 0,
-                        y: 0,
-                        w: imgData.width / 72,
-                        h: imgData.height / 72,
-                        sizing: {
-                            type: 'contain',
-                            w: MAX_IMAGE_WIDTH_PX / 72,
-                            h: MAX_IMAGE_HEIGHT_PX / 72
-                        }
-                    });
-                });
-            }
-        });
-
-        // add spacer
-        pptx.addNewSlide('MAIN');
-    });
-}
-
-/**
- * Numbers items in an array and joins with \n
- * @param {[string]} slides
- * @return {String}
- */
-function numberAndJoin(slides) {
-    const retArray = [];
-    slides.forEach((item, index) => {
-        retArray.push(`${index + 1}. ${item}`);
-    });
-    return retArray.join('\n');
-}
-
-// let's do this
-getDirectories((directories) => {
+function writePptx(slideMap) {
     // create a pptx
     const pptx = new PptxGenJs();
     pptx.setLayout('LAYOUT_WIDE');// 13.33 x 7.5 inches | 957.6 x 540 px
@@ -220,44 +153,93 @@ getDirectories((directories) => {
         bkgd: '000000'
     });
 
-    const allFilePromises = [];
-    const slideMap = new Map();
-    directories.forEach((category) => {
-        allFilePromises.push(new Promise((dirResolve, dirReject) => {
-            fs.readdir(path.join('.', category), (err, filenames) => {
+    let round = 1;
+
+    // slide = {slidePath: string; movieName: string;}
+    slideMap.forEach((slides, category) => {
+        // add round title
+        const titleSlide = pptx.addNewSlide('MAIN');
+        titleSlide.addText(`Round ${round++}: ${category}\nPoints for title, year, and director.`, {
+            align: 'center',
+            valign: 'middle',
+            fontFace: 'Arial',
+            fontSize: 60
+        });
+
+        slides.forEach((slide, slideIndex) => {
+            const slidePath = path.join('.', category, slide);
+            console.log(`${category}, ${slideIndex}: ${slidePath}`);
+            const slide = pptx.addNewSlide('MAIN');
+
+            // get image size synchronously. Probably slow, but easy to code.
+            const image = sharp(slidePath);
+            image.metadata((err, imgData) => {
                 if (err) {
-                    dirReject(err);
+                    console.error(err);
+                    process.exit();
                 }
 
-                // set a shiffled list of slides
-                const deDuped = removeDuplicates(filenames);
-                let shuffled = fyShuffle(deDuped); // shuffle the array for slide output
-                shuffled = limitArrayTo(shuffled, 15); // take the top x
-                const output = filterFilenames(shuffled); // clean up the names for bingo cards
-                slideMap.set(category, output);
-                dirResolve(slideMap);
-            });
-        }));
-    });
-
-
-    // wait to save until all are done.
-    Promise.all(allSlidePromises).then(() => {
-        writeSlideCallSheet(slideMap, () => {
-            writePptx(slideMap, pptx)
-                .then(() => {
-                    try {
-                        pptx.save(path.join('.', category, `NtF-${todayStamp}.pptx`), (filename) => {
-                            console.log(`Finishing up writing ${filename}, then exiting...`);
-                        });
-                    } catch (err) {
-                        console.error(err);
-                        process.exit(1);
+                // NOTE: we need to include the original width and height for the aspect ratio to be maintained
+                slide.addImage({
+                    path: slidePath,
+                    x: 0,
+                    y: 0,
+                    w: imgData.width / 72,
+                    h: imgData.height / 72,
+                    sizing: {
+                        type: 'contain',
+                        w: MAX_IMAGE_WIDTH_PX / 72,
+                        h: MAX_IMAGE_HEIGHT_PX / 72
                     }
                 });
+            });
         });
-    }, (err) => {
+
+        // add spacer
+        pptx.addNewSlide('MAIN');
+    });
+
+    return Promise.resolve(pptx);
+}
+
+// let's do this
+getDirectories()
+    .then(directories => {
+        const slideListPromises = [];
+        directories.forEach(category => {
+            slideListPromises.push(
+                fs.readdir(path.join('.', category))
+                    .then(filenames => {
+                        const deDuped = cleanUpFileList(filenames);
+                        // generate the full file path and human readable name for slide, and have that object be the 'slide'
+                        const slideList = limitArrayTo(deDuped.map(file => ({
+                            slidePath: path.join('.', category, file),
+                            movieName: filenameToMovie(file)
+                        })), 15);
+                        return [ category, fyShuffle(slideList) ];
+                    }))
+        });
+        return slideListPromises;
+    })
+    .catch(err => {
+        console.error('Error reading filenames: ', err);
+        process.exit(1);
+    })
+    .then(slideListPromises => {
+        const slideMap = new Map();
+        fyShuffle(slideListPromises).forEach(catMap => {
+            slideMap.set(catMap[ 0 ], catMap[ 1 ])
+        });
+
+        return writeSlideCallSheet(slideMap);
+    })
+    .then(writePptx)
+    .then(pptx => {
+        pptx.save(`NtF-${todayStamp}.pptx`, (filename) => {
+            console.log(`Finishing up writing ${filename}, then exiting...`);
+        });
+    })
+    .catch(err => {
         console.error(err);
         process.exit(1);
     });
-});
